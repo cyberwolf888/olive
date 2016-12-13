39c176438cb3585c4252d2ce7211614247047839
Karen <wijaya.imd@gmail.com>
2016-12-13 15:29:08 +0800

    olive 3.0

--- app/Http/Controllers/Master/ReportController.php
+++ app/Http/Controllers/Master/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Master;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -15,7 +16,27 @@ class ReportController extends Controller
 
     public function transaction(Request $request)
     {
-        return dd($request->all());
+        $this->validate($request, [
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'status' => 'required|numeric'
+        ]);
+        $start_date = date('Y/m/d', strtotime($request->start_date));
+        $end_date = date('Y/m/d', strtotime($request->end_date));
+        $transaction = Transaction::whereRaw('created_at >= "'.$start_date.'"')->whereRaw('created_at <= "'.$end_date.'"');
+        if($request->status != '6'){
+            $transaction->where('status', $request->status);
+        }
+
+        $total = $transaction->sum('total');
+        $model = $transaction->get();
+        $count = $transaction->count();
+        return view('master/report/result',[
+            'model'=>$model,
+            'start_date'=>$start_date,
+            'end_date'=>$end_date,
+            'total'=>$total
+        ]);
     }
 
 
