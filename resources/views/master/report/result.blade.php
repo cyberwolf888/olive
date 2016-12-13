57f753315faf7cfac354c7b94f6060393bd91bbe
Karen <wijaya.imd@gmail.com>
2016-12-13 15:41:24 +0800

    olive 3.1

--- resources/views/master/report/result.blade.php
+++ resources/views/master/report/result.blade.php
@@ -3,6 +3,7 @@
 @push('plugin_css')
 <link href="{{ url('assets/master') }}/plugins/sweetalert/sweetalert.css" rel="stylesheet" type="text/css"/>
 <link href="{{ url('assets/master') }}/plugins/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/buttons/1.2.3/css/buttons.dataTables.min.css" rel="stylesheet">
 @endpush
 
 @section('content')
@@ -14,7 +15,7 @@
             <div class="col s12 m12 l12">
                 <div class="card">
                     <div class="card-content">
-                        <span class="card-title">Report Period {{ date('d F Y',strtotime($start_date)) }} - {{ date('d F Y',strtotime($end_date)) }}</span>
+                        <span class="card-title">Report Periode {{ date('d F Y',strtotime($start_date)) }} - {{ date('d F Y',strtotime($end_date)) }}</span>
                         <table id="example" class="display responsive-table datatable-example">
                             <thead>
                             <tr>
@@ -64,11 +65,60 @@
 @push('plugin_scripts')
 <script src="{{ url('assets/master') }}/plugins/sweetalert/sweetalert.min.js"></script>
 <script src="{{ url('assets/master') }}/plugins/datatables/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.3/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.3/js/buttons.html5.min.js"></script>
 @endpush
 
 @push('scripts')
-<script src="{{ url('assets/master') }}/js/pages/table-data.js"></script>
 <script>
+    $(document).ready(function() {
+        $('#example').DataTable({
+            language: {
+                searchPlaceholder: 'Search records',
+                sSearch: '',
+                sLengthMenu: 'Show _MENU_',
+                sLength: 'dataTables_length',
+                oPaginate: {
+                    sFirst: '<i class="material-icons">chevron_left</i>',
+                    sPrevious: '<i class="material-icons">chevron_left</i>',
+                    sNext: '<i class="material-icons">chevron_right</i>',
+                    sLast: '<i class="material-icons">chevron_right</i>'
+                }
+            },
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    exportOptions: {
+                        columns: [ 0, 1, 2, 3, 4, 5 ]
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    exportOptions: {
+                        columns: [ 0, 1, 2, 3, 4, 5 ]
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    exportOptions: {
+                        columns: [ 0, 1, 2, 3, 4, 5 ]
+                    }
+                },
+                {
+                    extend: 'csvHtml5',
+                    exportOptions: {
+                        columns: [ 0, 1, 2, 3, 4, 5 ]
+                    }
+                }
+            ]
+        });
+        $('.dataTables_length select').addClass('browser-default');
+    });
+
     @if (session('success'))
         $(document).ready(function () {
         swal("Success!", "{{ session('success') }}", "success");
